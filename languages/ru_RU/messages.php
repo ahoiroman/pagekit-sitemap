180a6edb7288737f7d4fae40677a2bb0eff97f9f
Roman <roman.lossin@icloud.com>
2017-05-29 10:16:48 +0200

    - ADD: Russian translation (Thank you, Dmitry Aronov/Glaicer)

---
+++ languages/ru_RU/messages.php
@@ -0,0 +1,16 @@
+<?php return [
+	'Sitemap Settings'                                                                                   => 'Настройки приложения',
+	'Exclusions'                                                                                         => 'Исключения',
+	'Frequency'                                                                                          => 'Частота обновления',
+	'Daily'                                                                                              => 'Ежедневно',
+	'Weekly'                                                                                             => 'Еженедельно',
+	'Monthly'                                                                                            => 'Ежемесячно',
+	'Yearly'                                                                                             => 'Ежегодно',
+	'Filename'                                                                                           => 'Имя файла',
+	'Generate'                                                                                           => 'Генерировать',
+	'Generating'                                                                                         => 'Генерация',
+	'Add'                                                                                                => 'Добавить',
+	'You can add your first exclusion using the input field above. Go ahead!'                            => 'Вы можете добавить исключение используя форму ввода выше. Попробуйте!',
+	'Sitemap-generation is in progress. Please stand by until the "Sitemap generated"-message shows up.' => 'Идёт процесс генерации карты сайта. Пожалуйста, не закрывайте страницу до появления сообщения "Карта сайта сгенерирована"',
+	'Sitemap generated.'                                                                                 => 'Карта сайта сгенерирована.',
+];

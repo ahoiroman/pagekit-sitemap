b86af271d2947f37dcbbdbff532c89366af1b295
Roman <roman.lossin@icloud.com>
2017-12-12 22:56:25 +0100

    fixed formatting

--- index.php
+++ index.php
@@ -4,60 +4,60 @@ use Pagekit\Application;
 
 
 return [
-	'name' => 'sitemap',
-	'type' => 'extension',
-	'main' => function( Application $app ) {
-		// bootstrap code
-	},
-	
-	'autoload' => [
-		'Spqr\\Sitemap\\' => 'src'
-	],
-	
-	'nodes' => [],
-	
-	'routes' => [
-		'/sitemap' => [
-			'name'       => '@sitemap',
-			'controller' => [
-				'Spqr\\Sitemap\\Controller\\SitemapController'
-			]
-		],
-	],
-	
-	'menu' => [
-		'sitemap'           => [
-			'label'  => 'Sitemap',
-			'url'    => '@sitemap',
-			'active' => '@sitemap(/*)?',
-			'icon'   => 'sitemap:icon.svg',
-            'access' => 'sitemap: manage settings'
+    'name' => 'sitemap',
+    'type' => 'extension',
+    'main' => function (Application $app) {
+        // bootstrap code
+    },
+    
+    'autoload' => [
+        'Spqr\\Sitemap\\' => 'src',
+    ],
+    
+    'nodes' => [],
+    
+    'routes' => [
+        '/sitemap' => [
+            'name'       => '@sitemap',
+            'controller' => [
+                'Spqr\\Sitemap\\Controller\\SitemapController',
+            ],
         ],
-		'sitemap: settings' => [
-			'parent' => 'sitemap',
-			'label'  => 'Settings',
-			'url'    => '@sitemap/settings',
-			'access' => 'sitemap: manage settings'
-		]
-	],
-	
-	'permissions' => [
-		'sitemap: manage settings' => [
-			'title' => 'Manage settings'
-		]
-	],
-	
-	'settings' => '@sitemap/settings',
-	
-	'resources' => [
-		'sitemap:' => ''
-	],
-	
-	'config' => [
-		'frequency' => 'weekly',
-		'filename'  => 'sitemap.xml',
-		'excluded'  => []
-	],
-	
-	'events' => []
+    ],
+    
+    'menu' => [
+        'sitemap'           => [
+            'label'  => 'Sitemap',
+            'url'    => '@sitemap',
+            'active' => '@sitemap(/*)?',
+            'icon'   => 'sitemap:icon.svg',
+            'access' => 'sitemap: manage settings',
+        ],
+        'sitemap: settings' => [
+            'parent' => 'sitemap',
+            'label'  => 'Settings',
+            'url'    => '@sitemap/settings',
+            'access' => 'sitemap: manage settings',
+        ],
+    ],
+    
+    'permissions' => [
+        'sitemap: manage settings' => [
+            'title' => 'Manage settings',
+        ],
+    ],
+    
+    'settings' => '@sitemap/settings',
+    
+    'resources' => [
+        'sitemap:' => '',
+    ],
+    
+    'config' => [
+        'frequency' => 'weekly',
+        'filename'  => 'sitemap.xml',
+        'excluded'  => [],
+    ],
+    
+    'events' => [],
 ];
